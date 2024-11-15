ab75d8c7fba1de52a1144699106d84260425ea24
Artem <60689988+VLGKiwi@users.noreply.github.com>
2024-11-15 14:19:18 +0300

    fix: errors Conversion

--- src/modules/conversion/conversion.tsx
+++ src/modules/conversion/conversion.tsx
@@ -1,17 +1,11 @@
 'use client'
 
-import { FC, useRef } from 'react'
+import { FC } from 'react'
 import classNames from 'classnames'
 
 import styles from './conversion.module.scss'
 import { ConversionProps } from './conversion.types'
 import { ConversionTitle, ConversionType } from '@/components'
-import { useGSAP } from '@gsap/react'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import gsap from 'gsap'
-
-gsap.registerPlugin(ScrollTrigger)
-
 
 const Conversion: FC<ConversionProps> = ({
   className
