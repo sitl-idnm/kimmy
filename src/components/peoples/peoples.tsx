136cfd4fd3f633e5cf25038403d56f9a0b36b010
sitl-idnm <klevtsevitch.lev@yandex.ru>
2025-01-09 14:14:23 +0300

    Добавил смену изображений при хавере

--- src/components/peoples/peoples.tsx
+++ src/components/peoples/peoples.tsx
@@ -1,4 +1,6 @@
-import { FC } from 'react'
+'use client'
+
+import { FC, useState } from 'react'
 import classNames from 'classnames'
 
 import styles from './peoples.module.scss'
@@ -9,6 +11,8 @@ import { AnimatedImage, Button } from '@/ui'
 const Peoples: FC<PeoplesProps> = ({
   className
 }) => {
+  const [isFirstHovered, setIsFirstHovered] = useState(false);
+  const [isSecondHovered, setIsSecondHovered] = useState(false);
   const rootClassName = classNames(styles.root, className)
 
   return (
@@ -17,23 +21,36 @@ const Peoples: FC<PeoplesProps> = ({
         <h2 className={styles.peoples__title}>Узнайте больше про наш подход у&nbsp;основателей агентства</h2>
         <div className={`${styles.peoples__card} ${styles.peoples__card__left}`}>
           <div className={styles.peoples__card__info}>
-          <h3 className={styles.peoples__card__title}>Павел Коржуев</h3>
-          <p className={styles.peoples__card__text}>@k.kim</p></div>
+            <h3 className={styles.peoples__card__title}>Павел Коржуев</h3>
+            <p className={styles.peoples__card__text}>@k.kim</p></div>
           <Button value="Написать в Telegram" />
         </div>
         <div className={`${styles.peoples__card} ${styles.peoples__card__right}`}>
           <div className={styles.peoples__card__info}>
-          <h3 className={styles.peoples__card__title}>Константин Ким</h3>
-          <p className={styles.peoples__card__text}>@k.kim</p></div>
+            <h3 className={styles.peoples__card__title}>Константин Ким</h3>
+            <p className={styles.peoples__card__text}>@k.kim</p></div>
           <Button value="Написать в Telegram" />
         </div>
         <Image
-            src='/images/peoples.png'
-            width={910}
-            height={563}
-            quality={100}
-            alt='peoples'
-          className={styles.peoples__image} ></Image>
+          src={isFirstHovered ? '/images/first.png' : '/images/first__anon.png'}
+          width={500}
+          height={563}
+          quality={100}
+          alt='peoples'
+          className={styles.peoples__image__first}
+          onMouseEnter={() => setIsFirstHovered(true)}
+          onMouseLeave={() => setIsFirstHovered(false)}
+        />
+        <Image
+          src={isSecondHovered ? '/images/second.png' : '/images/second__anon.png'}
+          width={535}
+          height={545}
+          quality={100}
+          alt='peoples'
+          className={styles.peoples__image__second}
+          onMouseEnter={() => setIsSecondHovered(true)}
+          onMouseLeave={() => setIsSecondHovered(false)}
+        />
         <AnimatedImage />
       </div>
     </div>
