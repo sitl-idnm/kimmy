2ab1e886d28ef5b09d60a8c774c5e71856234076
Artem <60689988+VLGKiwi@users.noreply.github.com>
2025-01-09 16:47:29 +0300

    feat: Artem/add-page-services: end task

---
+++ src/views/servicePage/servicePage.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react'
+import classNames from 'classnames'
+
+import styles from './servicePage.module.scss'
+import { ServicePageProps } from './servicePage.types'
+
+const ServicePage: FC<ServicePageProps> = ({
+  className
+}) => {
+  const rootClassName = classNames(styles.root, className)
+
+  return (
+    <main className={rootClassName}>
+      <h1>ServicePage</h1>
+    </main>
+  )
+}
+
+export default ServicePage
---
+++ src/views/servicePage/servicePage.types.ts
@@ -0,0 +1,3 @@
+export interface ServicePageProps {
+  className?: string
+}
