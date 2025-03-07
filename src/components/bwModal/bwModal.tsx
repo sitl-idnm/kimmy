0fe429cf5ae91f2902e9b203842e28f0c35eeed8
Artem <zvonarev.dev@gmail.com>
2025-03-07 13:49:00 +0300

    fix

--- src/components/bwModal/bwModal.tsx
+++ src/components/bwModal/bwModal.tsx
@@ -1,15 +1,45 @@
 import { FC } from 'react'
-import { Modal } from '@/ui'
+import styles from './bwModal.module.scss'
+import Image from 'next/image'
 import { useSetAtom } from 'jotai'
 import { openModalContent } from '@/shared/atoms/openModal'
 
 const BwModal: FC = () => {
   const setModalContent = useSetAtom(openModalContent)
 
+  const closeModal = () => {
+    setModalContent('')
+  }
+
   return (
-    <Modal onClose={() => setModalContent(null)}>
-      {/* Modal content */}
-    </Modal>
+    <div className={styles.modal} onClick={(e) => {
+      if (e.target === e.currentTarget) {
+        closeModal()
+      }
+    }}>
+      <div className={styles.modal__content} onClick={(e) => e.stopPropagation()}>
+        <div className={styles.close} onClick={closeModal}></div>
+        <div className={styles.content}>
+          <div>
+            <Image
+              src={'/images/cases/bestwave/bwModal.png'}
+              alt='bwModal'
+              quality={100}
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{
+                width: '100%',
+                height: 'auto',
+                objectFit: 'contain',
+                maxWidth: '100%'
+              }}
+              priority
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
