1c3793ba55f2ac1eb69cada384a228eb2f3f6c01
sitl-idnm <klevtsevitch.lev@yandex.ru>
2025-03-18 16:05:39 +0300

    Исправил ошибку

--- src/components/favour/favour.tsx
+++ src/components/favour/favour.tsx
@@ -8,8 +8,8 @@ import { FavourItem } from '../favourItem'
 const itemsData = [
   { title: 'исследования', linkText: 'Подробнее', linkColor: 'var(--color-black)', backgroundColor: 'var(--color-grey)', textColor: 'var(--color-black)', imageSrc: '/images/image1.png', imageTitle: 'исследования' },
   { title: 'дизайн', linkText: 'Подробнее', linkColor: 'var(--color-white-default)', backgroundColor: 'var(--color-black)', textColor: 'var(--color-white-default)', imageSrc: '/images/image2.png', imageTitle: 'дизайн' },
-  { title: <>разработка<br />и поддержка</>, linkText: 'Подробнее', linkColor: 'var(--color-black)', backgroundColor: 'var(--color-grey-light)', textColor: 'var(--color-black)', imageSrc: '/images/image3.png', imageTitle: 'разработка' },
-  { title: 'поддержка', linkText: 'Подробнее', linkColor: 'var(--color-black)', backgroundColor: 'var(--color-red-accent)', textColor: 'var(--color-white-default)', imageSrc: '/images/image4.png', imageTitle: 'поддержка' },
+  { title: <>разработка<br />и поддержка</>, linkText: 'Подробнее', linkColor: 'var(--color-black)', backgroundColor: 'var(--color-grey-light)', textColor: 'var(--color-black)', imageSrc: '/images/image3.png', imageTitle: 'разработка' },
+  { title: 'поддержка', linkText: 'Подробнее', linkColor: 'var(--color-black)', backgroundColor: 'var(--color-red-accent)', textColor: 'var(--color-white-default)', imageSrc: '/images/image4.png', imageTitle: 'поддержка' }
 ]
 
 const Favour: FC<FavourProps> = ({
