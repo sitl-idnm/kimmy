c8b20f35451c70044311a2c6176373660c9129b2
sitl-idnm <klevtsevitch.lev@yandex.ru>
2025-08-25 16:54:32 +0300

    feat:/подключил Яндекс Метрику

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,11 @@ import { Header } from '@modules/header'
 import '@styles/global.scss'
 
 import localFont from 'next/font/local'
+import Script from 'next/script'
 import { Provider } from '@service/provider'
 import { CookieBanner } from '@/modules/cookieBanner'
+
+const YM_ID = '103883810'
 
 const inter = localFont({
   src: [
@@ -68,6 +71,26 @@ export default function RootLayout({
 
           <div id="modal-root" />
         </Provider>
+
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) { return; }
+              }
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=${YM_ID}', 'ym');
+
+            ym(${YM_ID}, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:'dataLayer', accurateTrackBounce:true, trackLinks:true});
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img src={`https://mc.yandex.ru/watch/${YM_ID}`} style={{ position: 'absolute', left: '-9999px' }} alt="" />
+          </div>
+        </noscript>
       </body>
     </html>
   )
