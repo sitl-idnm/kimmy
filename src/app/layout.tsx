0da1dcfa05e94da503452a9daab42542fbe3d13a
s0ys1516 <klevtsevitch.lev@yandex.ru>
2026-03-10 12:49:00 +0300

    feat:/ add pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,6 +65,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
+      <head>
+        <Script id="top-mail-ru" strategy="afterInteractive">
+          {`var _tmr = window._tmr || (window._tmr = []);
+_tmr.push({id: "3747425", type: "pageView", start: (new Date()).getTime()});
+(function (d, w, id) {
+  if (d.getElementById(id)) return;
+  var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+  ts.src = "https://top-fwz1.mail.ru/js/code.js";
+  var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+  if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+})(document, window, "tmr-code");`}
+        </Script>
+      </head>
       <body className={`${inter.variable} ${manrope.variable}`}>
         <Provider>
           <div id="root">
@@ -76,6 +89,11 @@ export default function RootLayout({
 
           <div id="modal-root" />
         </Provider>
+
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <div><img src="https://top-fwz1.mail.ru/counter?id=3747425;js=na" style={{ position: 'absolute', left: '-9999px' }} alt="Top.Mail.Ru" /></div>
+        </noscript>
 
         <Script id="yandex-metrika" strategy="afterInteractive">
           {`
